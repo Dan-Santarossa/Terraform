b3f8f50d2c0bf4e408a02c7ead4bd22d9b21ad81
Dan-Santarossa <dpsantarossa@gmail.com>
2022-08-02 23:56:26 -0400

    changed name of file/started project/

---
+++ main-project.tf
@@ -0,0 +1,35 @@
+terraform{
+    required_providers {
+    aws = {
+        source = "hashicorp/aws"
+        version = "~> 4.16"
+    }
+  }
+
+required_version = ">= 1.2.0"
+ 
+}
+resource "aws_vpc" "main" {
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "main"
+  }
+}
+resource "aws_subnet" "main" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "Main"
+  }
+}
+resource "aws_subnet" "main" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.2.0/24"
+
+  tags = {
+    Name = "Main"
+  }
+}
