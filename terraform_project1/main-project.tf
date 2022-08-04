b511658473198b040208c921e7c75b75e5f787d7
Dan-Santarossa <dpsantarossa@gmail.com>
2022-08-03 21:46:21 -0400

    test run for vpc = success

--- terraform_project1/main-project.tf
+++ terraform_project1/main-project.tf
@@ -1,37 +1,36 @@
-terraform{
-    required_providers {
+terraform {
+  required_providers {
     aws = {
-        source = "hashicorp/aws"
-        version = "~> 4.16"
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
     }
   }
 
-required_version = ">= 1.2.0"
- 
+  required_version = ">= 1.2.0"
+
 }
-##this block creates the vpc for the project
-resource "aws_vpc" "t7m-project-vpc" {
-  cidr_block       = "10.0.0.0/16"
-  instance_tenancy = "default"
-  
+##this block creates the vpc 
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+
   tags = {
     Name = "t7m-project-vpc"
   }
 }
 ##this block creates the first public subnet for vpc
 resource "aws_subnet" "public-subnet1" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = "10.0.1.0/24"
-  availability_zone = us-east-1a
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
   tags = {
     Name = "t7m-public-subnet1"
   }
 }
 ##this block creates second public subnet for vpc
 resource "aws_subnet" "public-subnet2" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = "10.0.2.0/24"
-  availability_zone = us-east-1b
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "us-east-1b"
   tags = {
     Name = "t7m-public-subnet2"
   }
