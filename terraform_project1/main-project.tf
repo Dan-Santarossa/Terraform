2eac5c2d17d8a2a31e249705b9b55210e82e497b
Dan-Santarossa <dpsantarossa@gmail.com>
2022-08-04 16:33:01 -0400

    route table and associations with subnet created

--- terraform_project1/main-project.tf
+++ terraform_project1/main-project.tf
@@ -1,6 +1,6 @@
 ##Two tier architecture in AWS 
 
-#Define the provider: Amazon Web Services
+##Define the provider: Amazon Web Services
 terraform {
   required_providers {
     aws = {
@@ -9,7 +9,6 @@ terraform {
     }
   }
   required_version = ">= 1.2.0"
-
 }
 ##this block creates the vpc 
 resource "aws_vpc" "t7m-project-vpc" {
@@ -20,7 +19,7 @@ resource "aws_vpc" "t7m-project-vpc" {
   }
 }
 ##this block creates the first public subnet
-resource "aws_subnet" "public-subnet1" {
+resource "aws_subnet" "t7m-public-subnet1a" {
   vpc_id                  = aws_vpc.t7m-project-vpc.id
   cidr_block              = "10.0.1.0/24"
   availability_zone       = "us-east-1a"
@@ -31,7 +30,7 @@ resource "aws_subnet" "public-subnet1" {
   }
 }
 ##this block creates second public subnet
-resource "aws_subnet" "public-subnet2" {
+resource "aws_subnet" "t7m-public-subnet1b" {
   vpc_id                  = aws_vpc.t7m-project-vpc.id
   cidr_block              = "10.0.2.0/24"
   availability_zone       = "us-east-1b"
@@ -65,15 +64,28 @@ resource "aws_subnet" "private-subnet2" {
 }
 ##this block creates an internet gateway
 resource "aws_internet_gateway" "t7m-ig" {
-  vpc_id  = aws_vpc.t7m-project-vpc.id
-  tags    = {
+  vpc_id = aws_vpc.t7m-project-vpc.id
+  tags = {
     Name = "t7m-ig"
   }
 }
-##this block creates our route table
+##this block creates our route table and associates it with our vpc
 resource "aws_route_table" "t7m-public-rt" {
   vpc_id = aws_vpc.t7m-project-vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.t7m-ig.id
+  }
   tags = {
     "Name" = "t7m-public-rt"
   }
+}
+##this block associates the route table to the public subnets
+resource "aws_route_table_association" "a" {
+  subnet_id      = aws_subnet.t7m-public-subnet1a.id
+  route_table_id = aws_route_table.t7m-public-rt.id
+}
+resource "aws_route_table_association" "b" {
+  subnet_id      = aws_subnet.t7m-public-subnet1b.id
+  route_table_id = aws_route_table.t7m-public-rt.id
 }
