60bddfd6a956cf8026c1e90052908cfc85464c6c
Dan-Santarossa <dpsantarossa@gmail.com>
2022-08-04 16:04:29 -0400

    added notes and changed the name of main vpc

--- terraform_project1/main-project.tf
+++ terraform_project1/main-project.tf
@@ -12,7 +12,7 @@ terraform {
 
 }
 ##this block creates the vpc 
-resource "aws_vpc" "main" {
+resource "aws_vpc" "t7m-project-vpc" {
   cidr_block = "10.0.0.0/16"
 
   tags = {
@@ -21,10 +21,10 @@ resource "aws_vpc" "main" {
 }
 ##this block creates the first public subnet
 resource "aws_subnet" "public-subnet1" {
-  vpc_id                  = aws_vpc.main.id
+  vpc_id                  = aws_vpc.t7m-project-vpc.id
   cidr_block              = "10.0.1.0/24"
   availability_zone       = "us-east-1a"
-  map_public_ip_on_launch = true
+  map_public_ip_on_launch = true ##creates a public Ip for the subnet
   tags = {
     Name = "t7m-public-subnet1a"
     Tier = "Public"
@@ -32,10 +32,10 @@ resource "aws_subnet" "public-subnet1" {
 }
 ##this block creates second public subnet
 resource "aws_subnet" "public-subnet2" {
-  vpc_id                  = aws_vpc.main.id
+  vpc_id                  = aws_vpc.t7m-project-vpc.id
   cidr_block              = "10.0.2.0/24"
   availability_zone       = "us-east-1b"
-  map_public_ip_on_launch = true
+  map_public_ip_on_launch = true ##creates a public Ip for the subnet
   tags = {
     Name = "t7m-public-subnet1b"
     Tier = "Public"
@@ -43,10 +43,10 @@ resource "aws_subnet" "public-subnet2" {
 }
 ##this block creates the first private subnet 
 resource "aws_subnet" "private-subnet1" {
-  vpc_id                  = aws_vpc.main.id
+  vpc_id                  = aws_vpc.t7m-project-vpc.id
   cidr_block              = "10.0.3.0/24"
   availability_zone       = "us-east-1c"
-  map_public_ip_on_launch = false
+  map_public_ip_on_launch = false ##does not create public Ip for the subnet - keeps it private
   tags = {
     Name = "t7m-private-subnet1c"
     Tier = "Private"
@@ -54,12 +54,20 @@ resource "aws_subnet" "private-subnet1" {
 }
 ##this block creates the second private subnet
 resource "aws_subnet" "private-subnet2" {
-  vpc_id                  = aws_vpc.main.id
+  vpc_id                  = aws_vpc.t7m-project-vpc.id
   cidr_block              = "10.0.4.0/24"
   availability_zone       = "us-east-1d"
-  map_public_ip_on_launch = false
+  map_public_ip_on_launch = false ##does not create public Ip for the subnet - keeps it private
   tags = {
     Name = "t7m-private-subnet1d"
     Tier = "Private"
   }
+}
+##this block will create our route table
+resource "aws_route_table" "t7m-public-rt" {
+  vpc_id = aws_vpc.t7m-project-vpc
+  tags = {
+    "Name" = "t7m-public-rt"
+  }
+  
 }
